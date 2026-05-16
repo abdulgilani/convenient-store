d8e73252b013adf31322f264e59e2257be4b7357
abdulgilani <ah-gilani@hotmail.com>
2026-05-16 16:38:11 -0400

    Prepared for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js";
@@ -9,14 +10,24 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173/",
-  }),
-);
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173/",
+    }),
+  );
+}
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/files/{/*path}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
